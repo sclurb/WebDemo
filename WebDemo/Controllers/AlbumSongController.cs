c2ac9828f15fedbe08b6fe2f69332f216bb48a48
Bob Donovan <Bob.Donovan@software-logistics.com>
2023-03-18 18:10:37 -0400

    Add project files.

---
+++ WebDemo/Controllers/AlbumSongController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDemo.Models;
+
+
+namespace WebDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumSongController : ControllerBase
+    {
+        // GET: api/<AlbumSongController>
+        [HttpGet("getAlbum")]
+        public Task<IActionResult> GetAlbum()
+        {
+            var album = new Album()
+            {
+                AlbumId = 1,
+                Title = "Goober",
+                ReleaseDate = "May 1999",
+                RecordedAt = "Special Studios",
+                Length = "3:47",
+                Producer = "Ted Templeman",
+                SucceededBy = "Goober II",
+                RecordedDate = "June 1987"
+            };
+            return Task.FromResult<IActionResult>(Ok(album));
+        }
+    }
+}
